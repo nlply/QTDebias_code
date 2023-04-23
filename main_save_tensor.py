b49ec29b93990e08c61e84eb7f39b4bb8fb5d985
nlply <yangliu.nlp@gmail.com>
2023-04-24 01:49:11 +0800

    优化方法

--- main_save_tensor.py
+++ main_save_tensor.py
@@ -1,10 +1,12 @@
 from datasets import load_dataset
 from tqdm.auto import tqdm
+import torch
 
 from transformers import BertTokenizer
 
 # 28996
-tokenizer = BertTokenizer.from_pretrained('qtbert', max_len=512)
+# tokenizer = BertTokenizer.from_pretrained('qtbert', max_len=512)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', max_len=512)
 tokens = tokenizer('I am a PhD')
 print(tokens)
 
@@ -37,8 +39,8 @@ print('decoding bias words...')
 # tokenizer.encode(['i','like','me'],add_special_tokens=False)
 attribute_words_tokenizer_id = tokenizer.encode(attribute_words,add_special_tokens=False)
 target_words_tokenizer_id = tokenizer.encode(target_words,add_special_tokens=False)
+all_special_ids = torch.tensor(tokenizer.all_special_ids)
 
-import torch
 attribute_words_tokenizer_id = torch.tensor(attribute_words_tokenizer_id)
 target_words_tokenizer_id = torch.tensor(target_words_tokenizer_id)
 
@@ -57,11 +59,11 @@ for i in range(input_ids.shape[0]):
     selection = torch.flatten(mask_arr[i].nonzero()).tolist()
     do_qt = False
     for mask_id in input_ids[i, selection]:
-        if mask_id in target_words_tokenizer_id:
+        if mask_id in target_words_tokenizer_id and mask_id not in all_special_ids:
             do_qt = True
     if do_qt:
         for j in range(input_ids[i].shape[0]):
-            if input_ids[i][j] in attribute_words_tokenizer_id:
+            if input_ids[i][j] in attribute_words_tokenizer_id and input_ids[i][j] not in input_ids[i][j]:
                 qt_mask[i,j] = 1
     # mask input_ids
     input_ids[i, selection] = 3  # our custom [MASK] token == 3
